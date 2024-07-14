5221d39b1ea0508f9fc3dcc2af1d9c4a62027885
118.hidalgo.leonel.1358 <118.hidalgo.leonel.1358@gmail.com>
2024-07-14 14:06:56 +0000

    Se agregan todos los setters y getters de courses

--- courses/course.go
+++ courses/course.go
@@ -6,7 +6,7 @@ type course struct {
 	name    string
 	price   float64
 	isFree  bool
-	UserIDs []uint
+	userIDs []uint
 	// Classes map[uint]string
 	classes []class
 }
@@ -31,6 +31,39 @@ func NewCourse(name string, price float64, isFree bool) (a *course) {
 	}
 }
 
+//! Según buenas prácticas de Go, lo setters llevan Set, pero los Getters solo
+//! se les pone el nombre de la variable a devolver en mayúscula
+
+// SetName() Setea el contenido de la variable name
+func (c *course) SetName(name string) { c.name = name }
+
+// Name() Retorna el contenido de la variable name
+func (c *course) Name() string { return c.name }
+
+// SetPrice() Setea el contenido de la variable price
+func (c *course) SetPrice(price float64) { c.price = price }
+
+// Price() Retorna el contenido de la variable price
+func (c *course) Price() float64 { return c.price }
+
+// SetIsFree() Setea el contenido de la variable isFree
+func (c *course) SetIsFree(isFree bool) { c.isFree = isFree }
+
+// IsFree() Retorna el contenido de la variable isFree
+func (c *course) IsFree() bool { return c.isFree }
+
+// SetUserIDs() Setea el contenido de la variable userIDs
+func (c *course) SetUserIDs(userIDs []uint) { c.userIDs = userIDs }
+
+// UserIDs() Retorna el contenido de la variable userIDs
+func (c *course) UserIDs() []uint { return c.userIDs }
+
+// SetClasses() Setea el contenido de la variable classes
+func (c *course) SetClasses(classes []class) { c.classes = classes }
+
+// Classes() Retorna el contenido de la variable classes
+func (c *course) Classes() []class { return c.classes }
+
 // PrintClasses() Recibe un objeto Course e imprime el nombre y descripción de sus clases
 func (c course) PrintClasses() {
 	fmt.Println("Las clases de este curso son:")
@@ -44,11 +77,6 @@ func (c course) PrintClasses() {
 // ChangePrice() Recibe un valor y lo actualiza en el precio
 func (c *course) ChangePrice(newPrice float64) {
 	c.price = newPrice
-}
-
-// GetPrice() Retorna el contenido de la variable price
-func (c *course) GetPrice() float64 {
-	return c.price
 }
 
 // AddClass() Recibe name y description de una clase para crearla y añadirla a la lista de clases
