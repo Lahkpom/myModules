d07b5fb040d78195d417d6d0cb4b07ff31f70943
118.hidalgo.leonel.1358 <118.hidalgo.leonel.1358@gmail.com>
2024-07-14 11:46:17 +0000

    Se hace performance al paquete courses

--- courses/course.go
+++ courses/course.go
@@ -3,12 +3,12 @@ package courses
 import "fmt"
 
 type course struct {
-	Name    string
-	Price   float64
-	IsFree  bool
+	name    string
+	price   float64
+	isFree  bool
 	UserIDs []uint
 	// Classes map[uint]string
-	Classes []class
+	classes []class
 }
 
 // El constructor devuelve un puntero para que pueda utilizarse en la versión original para efectuar cambios, sino
@@ -25,17 +25,16 @@ func NewCourse(name string, price float64, isFree bool) (a *course) {
 	// Esto crea el objeto y se lo cevuelve para la variable que lo recibe en el otro lado
 	// que lo está solicitando
 	return &course{
-		Name:    name,
-		Price:   price,
-		IsFree:  isFree,
-		UserIDs: make([]uint, 0),
+		name:   name,
+		price:  price,
+		isFree: isFree,
 	}
 }
 
 // PrintClasses() Recibe un objeto Course e imprime el nombre y descripción de sus clases
 func (c course) PrintClasses() {
 	fmt.Println("Las clases de este curso son:")
-	for _, class := range c.Classes {
+	for _, class := range c.classes {
 		fmt.Println(class.name)
 		fmt.Println(class.description)
 		fmt.Println("==================================")
@@ -44,11 +43,11 @@ func (c course) PrintClasses() {
 
 // ChangePrice() Recibe un valor y lo actualiza en el precio
 func (c *course) ChangePrice(newPrice float64) {
-	c.Price = newPrice
+	c.price = newPrice
 }
 
 // AddClass() Recibe name y description de una clase para crearla y añadirla a la lista de clases
 func (c *course) AddClass(name string, description string) {
 	newClass := NewClass(name, description)
-	c.Classes = append(c.Classes, *newClass)
+	c.classes = append(c.classes, *newClass)
 }
